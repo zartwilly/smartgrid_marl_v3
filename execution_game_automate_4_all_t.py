383ba82d6234a97c2303bf633c2e331dc82c4c54
Wilfried EHOUNOU <zartwilly@gmail.com>
2021-07-06 19:16:03 +0200

    AUTOMATE: execution des algorithmes force-brute(BF), Detection NAsh(NH) et LRI avec BF et NH s executant dans le meme fichier bf_nash.

--- execution_game_automate_4_all_t.py
+++ execution_game_automate_4_all_t.py
@@ -21,6 +21,8 @@ import lri_game_model_automate_4_all_t as autoLriGameModel
 import force_brute_game_model_automate_4_all_t as autoBfGameModel
 import detection_nash_game_model_automate_4_all_t as autoNashGameModel
 
+import bf_nash_game_modele_1t as bfNhGameModel
+
 from datetime import datetime
 from pathlib import Path
 
@@ -735,6 +737,242 @@ def execute_algos_used_Generated_instances_N_INSTANCES_MULTI(arr_pl_M_T_vars_ini
             
     print("NB_EXECUTION cpt={}".format(cpt))
 #_________       all ALGOs N instances MULTI_PROCESS : fin       ______________ 
+
+#_________       all BF, NH ALGOs N instances MULTI_PROCESS : debut         __________ 
+def execute_BF_NH_algos_used_Generated_instances_N_INSTANCES_MULTI(arr_pl_M_T_vars_init,
+                                            name_dir=None,
+                                            date_hhmm=None,
+                                            k_steps=None,
+                                            NB_REPEAT_K_MAX=None,
+                                            algos=None,
+                                            learning_rates=None,
+                                            pi_hp_plus=None,
+                                            pi_hp_minus=None,
+                                            a=1, b=1,
+                                            gamma_version=1,
+                                            used_instances=True,
+                                            used_storage_det=True,
+                                            manual_debug=False, 
+                                            criteria_bf="Perf_t", 
+                                            numero_instance=0,
+                                            debug=False):
+    """
+    execute algos by using generated instances if there exists or 
+        by generating new instances
+    
+    date_hhmm="1041"
+    algos=["LRI1"]
+    
+    """
+    # directory to save  execution algos
+    name_dir = "tests" if name_dir is None else name_dir
+    date_hhmm = datetime.now().strftime("%d%m_%H%M") \
+            if date_hhmm is None \
+            else date_hhmm
+    
+    # steps of learning
+    k_steps = 5 if k_steps is None else k_steps
+    fct_aux.NB_REPEAT_K_MAX = 3 if NB_REPEAT_K_MAX is None else NB_REPEAT_K_MAX
+    p_i_j_ks = [0.5, 0.5, 0.5]
+    
+    # list of algos
+    ALGOS = fct_aux.ALGO_NAMES_LRIx + fct_aux.ALGO_NAMES_DET \
+            + fct_aux.ALGO_NAMES_BF + fct_aux.ALGO_NAMES_NASH
+    algos = ALGOS if algos is None \
+                  else algos
+    # list of pi_hp_plus, pi_hp_minus
+    pi_hp_plus = [0.2*pow(10,-3)] if pi_hp_plus is None else pi_hp_plus
+    pi_hp_minus = [0.33] if pi_hp_minus is None else pi_hp_minus
+    # learning rate 
+    learning_rates = [0.01] \
+            if learning_rates is None \
+            else learning_rates # list(np.arange(0.05, 0.15, step=0.05))
+    
+    
+    
+    zip_pi_hp = list(zip(pi_hp_plus, pi_hp_minus))
+    
+    cpt = 0
+    algo_piHpPlusMinus_learningRate \
+            = it.product(algos, zip_pi_hp, learning_rates)
+    
+    Cx = dict(); 
+    C1 = None; C2 = None; C3 = None; C4 = None; C5 = None; C6 = None;  C7 = None;
+    profils_stabilisation_LRI2 = None; profils_NH = None; 
+    k_stop_learning_LRI2 = None; Perf_sum_Vi_LRI2 = None;
+    Perf_best_profils_bf = None; nb_best_profils_bf = None;
+    Perf_bad_profils_bf = None; nb_bad_profils_bf = None;
+    Perf_bad_profils_NH = None; nb_bad_profils_NH = None;
+    
+    for (algo_name, (pi_hp_plus_elt, pi_hp_minus_elt), 
+             learning_rate) in algo_piHpPlusMinus_learningRate:
+        
+        print("______ execution {}: {}, rate={}______".format(cpt, 
+                    algo_name, learning_rate))
+        cpt += 1
+        msg = "pi_hp_plus_"+str(pi_hp_plus_elt)\
+                       +"_pi_hp_minus_"+str(pi_hp_minus_elt)
+        path_to_save = os.path.join(name_dir, "simu_"+date_hhmm,
+                                    msg, algo_name
+                                    )
+        if algo_name == fct_aux.ALGO_NAMES_LRIx[0]:
+            # 0: LRI1
+            print("*** ALGO: {} *** ".format(algo_name))
+            utility_function_version = 1
+            path_to_save = os.path.join(name_dir, "simu_"+date_hhmm,
+                                    msg, algo_name, str(learning_rate)
+                                    )
+            Path(path_to_save).mkdir(parents=True, exist_ok=True)
+            arr_M_T_K_vars_LRI1, profils_stabilisation_LRI1, \
+            k_stop_learning_LRI1, bool_equilibrium_nash_LRI1, \
+            Perf_sum_Vi_LRI1 \
+                = autoLriGameModel\
+                    .lri_balanced_player_game_all_pijk_upper_08(
+                        arr_pl_M_T_vars_init.copy(),
+                        pi_hp_plus=pi_hp_plus_elt, 
+                        pi_hp_minus=pi_hp_minus_elt,
+                        a=a, b=b,
+                        gamma_version=gamma_version,
+                        k_steps=k_steps, 
+                        learning_rate=learning_rate,
+                        p_i_j_ks=p_i_j_ks,
+                        utility_function_version=utility_function_version,
+                        path_to_save=path_to_save, 
+                        manual_debug=manual_debug, dbg=debug)
+                    
+        elif algo_name == fct_aux.ALGO_NAMES_LRIx[1]:
+            # 1: LRI2
+            print("*** ALGO: {} *** ".format(algo_name))
+            utility_function_version = 2
+            path_to_save = os.path.join(name_dir, "simu_"+date_hhmm,
+                                    msg, algo_name, str(learning_rate)
+                                    )
+            Path(path_to_save).mkdir(parents=True, exist_ok=True)
+            arr_M_T_K_vars_LRI2, profils_stabilisation_LRI2, \
+            k_stop_learning_LRI2, bool_equilibrium_nash_LRI2, \
+            Perf_sum_Vi_LRI2 \
+                = autoLriGameModel\
+                    .lri_balanced_player_game_all_pijk_upper_08(
+                        arr_pl_M_T_vars_init.copy(),
+                        pi_hp_plus=pi_hp_plus_elt, 
+                        pi_hp_minus=pi_hp_minus_elt,
+                        a=a, b=b,
+                        gamma_version=gamma_version,
+                        k_steps=k_steps, 
+                        learning_rate=learning_rate,
+                        p_i_j_ks=p_i_j_ks,
+                        utility_function_version=utility_function_version,
+                        path_to_save=path_to_save, 
+                        manual_debug=manual_debug, dbg=debug)
+                                
+        elif algo_name in fct_aux.ALGO_NAMES_DET:
+            # 0: Selfish-DETERMINIST, 1: Systematic-DETERMINIST
+            print("*** ALGO: {} *** ".format(algo_name))
+            Path(path_to_save).mkdir(parents=True, exist_ok=True)
+            arr_M_T_K_vars_DET \
+                = autoDetGameModel.determinist_balanced_player_game(
+                             arr_pl_M_T_vars_init.copy(),
+                             pi_hp_plus=pi_hp_plus_elt, 
+                             pi_hp_minus=pi_hp_minus_elt,
+                             a=a, b=b,
+                             gamma_version=gamma_version,
+                             algo_name=algo_name,
+                             used_storage=used_storage_det,
+                             path_to_save=path_to_save, 
+                             manual_debug=manual_debug, dbg=debug)
+        
+        elif algo_name in fct_aux.ALGO_NAMES_BF \
+            or algo_name in fct_aux.ALGO_NAMES_NASH:
+            """
+            dico_profils_BF = {"nb_profils":,"profils":[], "Perfs":[]}
+            dico_best_profils_BF = {"nb_best_profils":,"profils":[], 
+                                    "Perfs":[],"nashs":[], Perfs_nash":[]}
+            dico_bad_profils_BF = {"nb_bad_profils":,"profils":[], 
+                                    "Perfs":[],"nashs":[], Perfs_nash":[]}
+            dico_best_profils_NH = {"nb_best_profils":0, "profils":[], "Perfs":[]}
+            dico_bad_profils_NH = {"nb_bad_profils":0, "profils":[], "Perfs":[]}
+            dico_mid_profils_NH = {"nb_mid_profils":0, "profils":[], "Perfs":[]}
+            """
+            
+            algo_names = fct_aux.ALGO_NAMES_BF + fct_aux.ALGO_NAMES_NASH
+            
+            dico_profils_BF, dico_profils_NH, \
+            dico_best_profils_BF, dico_bad_profils_BF, dico_mid_profils_BF, \
+            dico_best_profils_NH, dico_bad_profils_NH, dico_mid_profils_NH \
+               = bfNhGameModel.BF_NASH_balanced_player_game(
+                    arr_pl_M_T_vars_init=arr_pl_M_T_vars_init.copy(), 
+                    algo_names=algo_names,
+                    pi_hp_plus=pi_hp_plus_elt, 
+                    pi_hp_minus=pi_hp_minus_elt,
+                    a=a, b=b,
+                    gamma_version=gamma_version,
+                    path_to_save=path_to_save, 
+                    name_dir=name_dir, 
+                    date_hhmm=date_hhmm,
+                    manual_debug=manual_debug, 
+                    criteria_bf=criteria_bf, dbg=debug)
+               
+            nb_best_profils_bf = dico_best_profils_BF["nb_best_profils"]
+            Perf_best_profils_bf = dico_best_profils_BF["Perfs"]
+            nb_bad_profils_bf = dico_bad_profils_BF["nb_bad_profils"]
+            Perf_bad_profils_bf = dico_bad_profils_BF["Perfs"]
+            
+            profils_NH = dico_profils_NH["profils"]
+            nb_bad_profils_NH = dico_bad_profils_NH["nb_bad_profils"]
+            Perf_bad_profils_NH = dico_bad_profils_NH["Perfs"]
+            nb_best_profils_NH = dico_best_profils_NH["nb_best_profils"]
+            Perf_best_profils_NH = dico_best_profils_NH["Perfs"]
+        
+    print("profils_stabilisation_LRI2={}, set_profils_NH={}, profils_NH={}, nb_best_profils_bf={}".format(
+            profils_stabilisation_LRI2, len(set(profils_NH)), len(profils_NH), 
+            nb_best_profils_bf  ))
+    
+    C1 = True if len(profils_NH) > 0 else False
+    C2 = True if k_stop_learning_LRI2 < k_steps else False
+    C4 = k_stop_learning_LRI2 if C2 else None
+    if C1 and C2:
+        if tuple(profils_stabilisation_LRI2) in profils_NH:
+            C3 = True
+        elif tuple(profils_stabilisation_LRI2) not in profils_NH:
+            C3 = False
+    C5 = Perf_sum_Vi_LRI2
+    C6 = Perf_best_profils_bf[0] if nb_best_profils_bf > 0 else None
+    C9 = Perf_bad_profils_bf[0] if nb_bad_profils_bf > 0 else None
+    if C1:
+        C7 = Perf_bad_profils_NH[0] if nb_bad_profils_NH > 0 else None
+        
+    check_C5_inf_C6 = None
+    if C5 is not None and C6 is not None and C5 <= C6:
+        check_C5_inf_C6 = "OK"
+    else:
+        check_C5_inf_C6 = "NOK"
+    check_C7_inf_C6 = None
+    if C7 is not None and C6 is not None and C7 <= C6:
+        check_C7_inf_C6 = "OK"
+    else:
+        check_C7_inf_C6 = "NOK"
+            
+    
+    
+    Cx={fct_aux.name_cols_CX["C1"]:[C1], fct_aux.name_cols_CX["C2"]:[C2], 
+        fct_aux.name_cols_CX["C3"]:[C3], fct_aux.name_cols_CX["C4"]:[C4], 
+        fct_aux.name_cols_CX["C5"]:[C5], fct_aux.name_cols_CX["C6"]:[C6], 
+        fct_aux.name_cols_CX["C7"]:[C7], fct_aux.name_cols_CX["C9"]:[C9],
+        fct_aux.name_cols_CX["check_C5_inf_C6"]:[check_C5_inf_C6], 
+        fct_aux.name_cols_CX["check_C7_inf_C6"]:[check_C7_inf_C6]}
+    
+    path_to_save = name_dir.split(os.sep)[0:2]
+    path_to_save.append("save_all_instances")
+    path_to_save = os.path.join(*path_to_save)
+    Path(path_to_save).mkdir(parents=True, exist_ok=True)
+        
+    pd.DataFrame(Cx,index=["instance"+str(numero_instance)]).to_csv(
+        os.path.join(path_to_save, "Cx_instance"+str(numero_instance)+".csv")
+        )
+            
+    print("NB_EXECUTION cpt={}".format(cpt))
+#_________       all BF, NH ALGOs N instances MULTI_PROCESS : fin       ______________ 
+
 
 
 #_________                      One algo: debut                 _______________
